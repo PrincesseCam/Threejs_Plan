8dd5598ba372000dc07e6b641eeded9e8d1cbb5f
PrincesseCam <kaborecam@gmail.com>
2022-06-13 12:35:06 +0900

    Add new files

---
+++ project.js
@@ -0,0 +1,6 @@
+// Find the latest version by visiting https://cdn.skypack.dev/three.
+  
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+  
+const scene = new THREE.Scene();
+console.log(scene);
